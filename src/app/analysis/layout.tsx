d1b84c23680d54f9f3a7ae2d4918eb1755e3de7e
James Padolsey <github@padolsey.net>
2025-07-08 18:43:26 +0800

    attempt fix

--- src/app/analysis/layout.tsx
+++ src/app/analysis/layout.tsx
@@ -1,3 +1,27 @@
-import { default as FullLayout } from '../(full)/layout';
+import { Suspense } from 'react';
+import { SiteFooter } from '@/app/components/SiteFooter';
+import { SiteHeader } from '@/app/components/SiteHeader';
+import { TopProgressBar } from '@/app/components/TopProgressBar';
+import { NavigationEvents } from '@/app/components/NavigationEvents';
 
-export default FullLayout; 
+export default function FullWidthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Suspense fallback={null}>
+        <TopProgressBar />
+        <NavigationEvents />
+      </Suspense>
+      <SiteHeader contentMaxWidth="max-w-[1800px]" />
+      <main className="flex-grow w-full bg-background text-foreground">
+        <div className="max-w-[1800px] mx-auto">
+            {children}
+        </div>
+      </main>
+      <SiteFooter contentMaxWidth="max-w-[1800px]" />
+    </div>
+  );
+} 
